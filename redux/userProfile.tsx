229da11a133867d0e6853cabd2114f29f6607455
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-26 11:50:57 +0530

    add redux

---
+++ redux/userProfile.tsx
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface AuthState {
+  isAuthenticated: boolean;
+  name: string;
+  email: string;
+  type: string;
+}
+
+const initialState: AuthState = {
+  isAuthenticated: false,
+  email: "",
+  name: "",
+  type: "",
+};
+
+export const authSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    profile: (state, action) => {
+      state.isAuthenticated = true;
+      state.email = action.payload.email;
+      state.name = action.payload.name;
+      state.type = action.payload.type;
+    },
+    logout: (state) => {
+      localStorage.removeItem("token");
+      state.isAuthenticated = false;
+      state.email = "";
+      state.name = "";
+      state.type = "";
+    },
+  },
+});
+
+export const { logout, profile } = authSlice.actions;
+export default authSlice.reducer;
