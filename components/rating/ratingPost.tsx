d51ea7e91fdbdb3e7f4512d8511ae4496cb390c8
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-26 13:41:03 +0530

    add component to display rating and post rating

---
+++ components/rating/ratingPost.tsx
@@ -0,0 +1,11 @@
+export const PostRating = ({
+  id,
+  isAuth,
+}: {
+  id: string;
+  isAuth: boolean;
+}) => {
+    return(
+        <h1>q</h1>
+    )
+};
---
+++ components/rating/showRating.tsx
@@ -0,0 +1,5 @@
+export const ShowRating = ({ id }: { id: string }) => {
+    return(
+        <h1>q</h1>
+    )
+};
