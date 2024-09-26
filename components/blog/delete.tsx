698390aeca707a80895fa82b57d141e5da4bd42f
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-26 11:42:27 +0530

    add delete blog component

---
+++ components/blog/delete.tsx
@@ -0,0 +1,36 @@
+import { axiosInstance } from "@/helper/axiosInstance";
+
+export const DeleteBlog = ({ id }: { id: string }) => {
+  const deleteBlog = async () => {
+    const token = localStorage.getItem("token");
+
+    try {
+      const res = await axiosInstance.delete(`/blog/delete/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (res.status === 200) {
+        alert(res?.data?.message);
+        window.location.reload();
+      }
+    } catch (err: any) {
+      alert(err?.response?.data?.message);
+    }
+  };
+
+  const confirmDelete = () => {
+    if (confirm("Are you sure you want to delete this blog?")) {
+      deleteBlog();
+    }
+  };
+  return (
+    <button
+      className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+      onClick={confirmDelete}
+    >
+      Delete
+    </button>
+  );
+};
