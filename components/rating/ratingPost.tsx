1a67787ebf2f5c2686b9b00c12229d8013e9270c
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-26 13:46:18 +0530

    integrate api in display and post rating

--- components/rating/ratingPost.tsx
+++ components/rating/ratingPost.tsx
@@ -1,11 +1,51 @@
-export const PostRating = ({
-  id,
-  isAuth,
-}: {
-  id: string;
-  isAuth: boolean;
-}) => {
-    return(
-        <h1>q</h1>
-    )
+import { axiosInstance } from "@/helper/axiosInstance";
+import { useState } from "react";
+
+export const PostRating = ({ id, isAuth }: { id: string; isAuth: boolean }) => {
+  const [ratingInput, setratingInput] = useState("");
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const sendData = {
+        blog_id: id,
+        rating: ratingInput,
+      };
+      const res = await axiosInstance.post("/rating/create", sendData, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      if (res.status == 200) {
+        setratingInput("");
+        alert(res?.data?.message);
+        window.location.reload();
+      }
+    } catch (err: any) {
+      console.log(err?.response?.data?.message);
+      alert(err?.response?.data?.message);
+    }
+  };
+  return (
+    <div id="comment-post">
+      <input
+        type="text"
+        name="rate"
+        id="rate"
+        value={ratingInput}
+        onChange={(e) => setratingInput(e.target.value)}
+        placeholder="Your rate"
+        className="border-b-2 border-indigo-500 outline-none w-full text-xl"
+        disabled={!isAuth}
+      />
+      {!isAuth && <p>You must be logged in to post rating</p>}
+      <button
+        type="submit"
+        onClick={handleSubmit}
+        className="mt-4 border border-gray-600 bg-gray-600 text-white rounded-lg px-4 py-2"
+        disabled={!isAuth}
+      >
+        Rate
+      </button>
+    </div>
+  );
 };
--- components/rating/showRating.tsx
+++ components/rating/showRating.tsx
@@ -1,5 +1,28 @@
+import { axiosInstance } from "@/helper/axiosInstance";
+import { useEffect, useState } from "react";
+
 export const ShowRating = ({ id }: { id: string }) => {
-    return(
-        <h1>q</h1>
-    )
+  const [rating, setRating] = useState([]);
+  const displayRating = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      const res = await axiosInstance.get(`/rating/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (res.status === 200) {
+        setRating(res.data.data);
+      }
+    } catch (err: any) {
+      console.log(err?.response?.data?.message);
+      //   alert(err?.response?.data?.message);
+    }
+  };
+
+  useEffect(() => {
+    displayRating();
+  }, []);
+
+  return <h1>q</h1>;
 };
