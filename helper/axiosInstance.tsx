e2ee263977866d2560a5818a83c7e936c6368194
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-26 23:21:08 +0530

    add production url

--- helper/axiosInstance.tsx
+++ helper/axiosInstance.tsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-
 export const axiosInstance = axios.create({
-  baseURL: "http://laravel_blog_post.test/api",
+  baseURL: "http://206.1.58.168/api",
   timeout: 1000,
   headers: {
     Accept: "application/json",
