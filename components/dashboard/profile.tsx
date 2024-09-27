a4e92f2c9534fcf20530f0b9d1655bae31d41e10
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-27 22:16:20 +0530

    add profile card component

--- components/dashboard/profile.tsx
+++ components/dashboard/profile.tsx
@@ -58,21 +58,63 @@ export const Profile = () => {
   }, []);
 
   return (
-    <div className="flex ml-52">
-      <h1>Profile</h1>
-      {user && (
-        <div>
-          <p>Name: {user?.name}</p>
-          <p>Email: {user?.email}</p>
-          <p>Type: {user?.type}</p>
-          <Image
-            src={user?.profile_image}
-            width={100}
-            height={100}
-            alt="Picture of the author"
-          />
-        </div>
-      )}
+    // <div classNameName="flex ml-52">
+    //   <h1>Profile</h1>
+    //   {user && (
+    //     <div>
+    //       <p>Name: {user?.name}</p>
+    //       <p>Email: {user?.email}</p>
+    //       <p>Type: {user?.type}</p>
+    //       <Image
+    //         src={user?.profile_image}
+    //         width={100}
+    //         height={100}
+    //         alt="Picture of the author"
+    //       />
+    //     </div>
+    //   )}
+    // </div>
+
+    <div className="bg-white overflow-hidden shadow rounded-lg border">
+      <div className="px-4 py-5 sm:px-6">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">
+          User Profile
+        </h3>
+        <p className="mt-1 max-w-2xl text-sm text-gray-500">
+          This is some information about the user.
+        </p>
+      </div>
+      <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+        {user && (
+          <dl className="sm:divide-y sm:divide-gray-200">
+            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Profile image</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                <Image
+                  src={user?.profile_image}
+                  width={100}
+                  height={100}
+                  alt="Picture of the author"
+                />
+              </dd>
+            </div>
+            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Full name</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {user.name}
+              </dd>
+            </div>
+            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">
+                Email address
+              </dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {user.email}
+              </dd>
+            </div>
+          </dl>
+        )}
+      </div>
     </div>
   );
 };
