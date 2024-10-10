1ab6bdbb93898c9ed8ea6ba51d02f5242776b087
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-10 18:09:56 +0530

    fix: change the base URL

--- helper/axiosInstance.tsx
+++ helper/axiosInstance.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://laravel_blog_post.test/api",
+  baseURL: "http://laravel-blog-backend.test/api",
   // baseURL: "http://206.1.58.168/api",
-  timeout: 1000,
+  // timeout: 1000,
   headers: {
     Accept: "application/json",
   },
